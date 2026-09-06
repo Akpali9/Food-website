ecd26c827223824c2aeb7c3087cc99fe9755c7e8
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-09-06 11:02:04 +0100

    Add type definitions for navigation parameters

---
+++ src/navigation/types.ts
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  MainTabs: undefined;
+  GoalDetail: { id: string };
+  NewGoal: undefined;
+  Templates: undefined;
+  Community: undefined;
+  Settings: undefined;
+  Install: undefined;
+};
+
+export type TabParamList = {
+  Dashboard: undefined;
+  Goals: undefined;
+  Daily: undefined;
+  Weekly: undefined;
+  More: undefined;
+};
